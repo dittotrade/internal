6917e114cb03c265652f1f620951b889538204e7
okharch <Oleksandr.Kharchenko@halliburton.com>
2022-08-29 11:48:10 +0300

    internal added db/test_utils 1.0

---
+++ db/test_utils.go
@@ -0,0 +1,73 @@
+package db
+
+import (
+	"fmt"
+)
+
+type (
+	ExchangeAccount struct {
+		Name  string
+		Eid   int32
+		Login int64
+		User  *User
+	}
+	User struct {
+		Name  string
+		Email string
+	}
+	TradingAccount struct {
+		Name           string
+		EchangeAccount *ExchangeAccount
+		StopLoss       float64
+		Equity         float64
+	}
+	DBMockup struct {
+		ExchangeAccounts []*ExchangeAccount
+		Users            []*User
+		TradingAccounts  []*TradingAccount
+		// private mechanics
+		cc                  int
+		transactionUnderway bool
+	}
+)
+
+func (dbm *DBMockup) NewTradingAccount() *TradingAccount {
+	if !dbm.transactionUnderway {
+		dbm.cc++
+		dbm.transactionUnderway = true
+		defer func() { dbm.transactionUnderway = false }()
+	}
+	var tradingAccount TradingAccount
+	tradingAccount.Name = fmt.Sprintf("investment %02d", dbm.cc)
+	tradingAccount.EchangeAccount = dbm.NewExchangeAccount()
+	dbm.TradingAccounts = append(dbm.TradingAccounts, &tradingAccount)
+	return &tradingAccount
+}
+
+func (dbm *DBMockup) NewUser() *User {
+	if !dbm.transactionUnderway {
+		dbm.cc++
+		dbm.transactionUnderway = true
+		defer func() { dbm.transactionUnderway = false }()
+	}
+	var user User
+	user.Name = fmt.Sprintf("user %02d", dbm.cc)
+	user.Email = fmt.Sprintf("user%02d@ditto.trade", dbm.cc)
+	dbm.Users = append(dbm.Users, &user)
+	return &user
+}
+
+func (dbm *DBMockup) NewExchangeAccount() *ExchangeAccount {
+	if !dbm.transactionUnderway {
+		dbm.cc++
+		dbm.transactionUnderway = true
+		defer func() { dbm.transactionUnderway = false }()
+	}
+	var ea ExchangeAccount
+	ea.Name = fmt.Sprintf("a %02d", dbm.cc)
+	ea.Eid = int32(dbm.cc)
+	ea.Login = int64(dbm.cc)
+	ea.User = dbm.NewUser()
+	dbm.ExchangeAccounts = append(dbm.ExchangeAccounts, &ea)
+	return &ea
+}
